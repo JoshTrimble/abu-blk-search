4f52ee9d1594ff40a1b32968ccf9917215ba0b67
Josh Trimble <62977765+JoshTrimble@users.noreply.github.com>
2022-03-15 21:09:45 -0600

    Update injection.js

--- injection.js
+++ injection.js
@@ -8,7 +8,6 @@ document.getElementById('cardName-collapsable').innerHTML = '<textarea id="deckl
 document.getElementById('decklist').value = getStore('currentList')
 
 const nextCard = getStore('cardArr') != getStore('cardsToSearch').length-1 ? `href="/magic-the-gathering/singles?search=${getStore('cardsToSearch')[getStore('cardArr')+1]}"` : 'href="https://abugames.com/cartview/shop"'
-
 const prevCard = getStore('cardArr') == 0 ?  `href="/magic-the-gathering/singles?search=${getStore('cardsToSearch')[getStore('cardArr')]}"` : `href="/magic-the-gathering/singles?search=${getStore('cardsToSearch')[getStore('cardArr')-1]}"` 
 
 function subForm() {
@@ -20,8 +19,8 @@ function subForm() {
     setStore('currentList', document.getElementById('decklist').value)
 
     //splits cards by line and adds only names to "cardsToSearch"
-    let splitCards = document.getElementById('decklist').value.split('\n')
-    setStore('cardsToSearch', splitCards.map(x => x.replace(/[0-9]/g, '').trim()))
+    setStore('cardQty', document.getElementById('decklist').value.split('\n'))
+    setStore('cardsToSearch', getStore('cardQty').map(x => x.replace(/[0-9]/g, '').trim()))
 
     setStore('cardArr', 0)
     window.location.href = `https://abugames.com/magic-the-gathering/singles?search=${getStore('cardsToSearch')[getStore('cardArr')]}`
@@ -30,10 +29,12 @@ function subForm() {
 //Adds previous and next card tabs if search has innitialized 
 if (getStore('cardArr') >= 0) {
     document.getElementsByClassName('main-nav')[0].innerHTML = `<ul _ngcontent-xpd-c138="" class="mainHere" style="width:715px"><li _ngcontent-xpd-c138="" style="width: 50%; text-align: center"><a _ngcontent-xpd-c138="" onclick="previous()" ${prevCard}>&lt; Previous Card</a></li><li _ngcontent-xpd-c138="" style="width: 50%; text-align: center"><a _ngcontent-xpd-c138="" onclick="next()" ${nextCard}>Next Card &gt;</a></li></ul>`
+    document.querySelector('h3[class="block-title"]').textContent = getStore('cardQty')[getStore('cardArr')]
+
 }
 
 function next() {
     setStore('cardArr', getStore('cardArr') + 1) }
 
 function previous() {
-    setStore('cardArr', getStore('cardArr') - 1) }
+    getStore('cardArr') == 0 ? null: setStore('cardArr', getStore('cardArr') - 1) }
